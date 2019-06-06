144ef5f3b5f5568f528af9d42696a179139d54f7
Adriana Guerra <adriana.luz.guerra@gmail.com>
2019-06-06 21:37:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,6 +26,7 @@ def monopoly_with_third_tier
 	    names: {
 	      reading_railroad: {},
 	      pennsylvania_railroad: {},
+	      b_and_o_railraod: {},
 	      shortline_railroad: {}
 	    }, 
 	    rent_in_dollars: {
