64daa5f60ade7eed2232a62ec8b11a57388ae71f
Adriana Guerra <adriana.luz.guerra@gmail.com>
2019-06-06 21:36:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,11 @@ def monopoly_with_third_tier
 	  railroads: 
 	  {
 	    pieces: 4, 
-	    names: {}, 
+	    names: {
+	      reading_railroad: {},
+	      
+	      
+	    }, 
 	    rent_in_dollars: {
 	      one_piece_owned: 25,
 	      two_pieces_owned: 50,
